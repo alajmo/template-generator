e35e3950475666815e4a683a0dccd7dc72f84644
samiralajmovic <alajmovic.samir@gmail.com>
2017-11-04 12:09:28 +0100

    test: Add trap cleanup

--- tests/tests.sh
+++ tests/tests.sh
@@ -7,21 +7,26 @@ BOILERPLATES_PATH="$TEST_DIR/boilerplates/"
 ACTUAL_OUTPUT_DIR="$TEST_DIR/actual-output"
 EXPECTED_OUTPUT_DIR="$TEST_DIR/expected-output"
 
+_cleanup() {
+  export BOILERPLATES_PATH=""
+  rm "$ACTUAL_OUTPUT_DIR" -irf
+}
+trap cleanup EXIT
+
 source "$TEST_DIR/assert.sh" # Format: assert <command> [stdout] [stdin]
 
 _pre_test() {
   export BOILERPLATES_PATH="$BOILERPLATES_PATH"
   mkdir -p "$ACTUAL_OUTPUT_DIR"
 }
-_post_test() {
-  export BOILERPLATES_PATH=""
-  rm "$ACTUAL_OUTPUT_DIR" -irf
-}
 
 _generate_tests() {
-  . ./tp generate "g-1.txt" "$ACTUAL_OUTPUT_DIR"
+  source ./tp generate "g-1.txt" "$ACTUAL_OUTPUT_DIR"
+  assert "diff $ACTUAL_OUTPUT_DIR/g-1.txt $EXPECTED_OUTPUT_DIR/g-1.txt" "" "Should generate boilerplate"
 
-  assert "diff \"$ACTUAL_OUTPUT_DIR/g-1.txt\" \"$EXPECTED_OUTPUT_DIR/g-1.txt\"" "" "Should generate boilerplate"
+  source ./tp generate "vs-1.txt" "$ACTUAL_OUTPUT_DIR"
+  assert "diff $ACTUAL_OUTPUT_DIR/vs-1.txt $EXPECTED_OUTPUT_DIR/vs-1.txt" "" "Should generate variable substituted boilerplate"
+
   assert_end "generate tests"
 }
 
@@ -39,7 +44,7 @@ _main() {
 
   _generate_tests
 
-  _post_test
+  _cleanup
 
 }
 _main "${@:-}"
