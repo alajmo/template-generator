73fa5d681df6e74ae2cd401381801a966bad5a12
samiralajmovic <alajmovic.samir@gmail.com>
2019-09-29 00:55:22 +0200

    feat: wip

--- bash-completion.sh
+++ bash-completion.sh
@@ -23,14 +23,39 @@ _get_boilerplates_path() {
 }
 
 ## tp [tab] => tp [commands] -> tp n|g|l|p|e
+## tp l[tab] => tp list [tab-> $BOILERPLATES_PATH]
 ## tp n[tab] => tp new [tab-> $PWD] [tab-> $BOILERPLATE_PATH]
-## tp g[tab] => tp generate [tab-> $BOILERPLATES_PATH] [tab-> $PWD]
-## tp l[tab] => tp l [tab-> $BOILERPLATES_PATH]
-## tp p[tab] => tp preview [tab-> $BOILERPLATES_PATH]
 ## tp e[tab] => tp edit [tab-> $BOILERPLATES_PATH]
+## tp p[tab] => tp preview [tab-> $BOILERPLATES_PATH]
+## tp g[tab] => tp generate [tab-> $BOILERPLATES_PATH] [tab-> $PWD]
 
 # File completion
 _tp() {
+    COMPREPLY=()
+    COMPREPLY+=($(compgen -W "list" "${COMP_WORDS[1]}"))
+    COMPREPLY+=($(compgen -W "new" "${COMP_WORDS[1]}"))
+
+    cur=`_get_cword`
+    prev=${COMP_WORDS[COMP_CWORD-1]}
+    _expand || return 0
+
+    # echo "$cur" > lala
+    # echo "$prev" >> lala
+
+    # ${COMP_WORDS[COMP_CWORD-1]}
+    if [[ $cur == "list" ]]; then
+        _filedir
+    fi
+
+    # _filedir
+    # COMPREPLY+=("list")
+    # COMPREPLY+=("new")
+    # COMPREPLY+=("edit")
+    # COMPREPLY+=("preview")
+    # COMPREPLY+=("generate")
+
+    return
+
     local cur;
     _get_comp_words_by_ref cur;
 
@@ -64,5 +89,6 @@ _tp() {
 
     COMPREPLY=(${_compreply[@]/$base_path_escaped/})
 }
+
 
 complete -o nospace -F _tp tp
